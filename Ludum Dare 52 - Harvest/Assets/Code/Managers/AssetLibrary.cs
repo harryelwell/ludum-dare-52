eb22d87d6647e15dec97f3b1fbb9ae92f65d60e4
harryelwell <107930302+harryelwell@users.noreply.github.com>
2023-01-07 22:43:56 +0000

    Corn Prefab
    Corn prefab is fully functional, complete with particles, growth coroutine, etc.

---
+++ Ludum Dare 52 - Harvest/Assets/Code/Managers/AssetLibrary.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetLibrary : MonoBehaviour
+{
+    [Header("Corn Sprites")]
+    public Sprite cornBase;
+    public Sprite cornShort;
+    public Sprite cornLong;
+}
---
+++ Ludum Dare 52 - Harvest/Assets/Code/Managers/CornManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornManager : MonoBehaviour
+{
+    public AssetLibrary assetLibrary;
+    public SpriteRenderer spriteRenderer;
+    public ParticleSystem cornParticle;
+    public int cornLevel; // 0 = cut, 1 = base, 2 = short, 3 = long
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCornlevel(Random.Range(0,4));
+
+        if(cornLevel > 0 && cornLevel < 3)
+        {
+            StartCoroutine(GrowCorn());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetCornlevel(int newLevel)
+    {
+        // change the int
+        cornLevel = newLevel;
+
+        // update the sprite
+        if(newLevel == 0)
+        {
+            spriteRenderer.color = new Color(spriteRenderer.color.r,spriteRenderer.color.g,spriteRenderer.color.b,0);
+            StartCoroutine(GrowCorn());
+        }
+
+        if(newLevel == 1)
+        {
+            spriteRenderer.sprite = assetLibrary.cornBase;
+            spriteRenderer.color = new Color(spriteRenderer.color.r,spriteRenderer.color.g,spriteRenderer.color.b,1);
+        }
+
+        if(newLevel == 2)
+        {
+            spriteRenderer.sprite = assetLibrary.cornShort;
+            spriteRenderer.color = new Color(spriteRenderer.color.r,spriteRenderer.color.g,spriteRenderer.color.b,1);
+        }
+
+        if(newLevel == 3)
+        {
+            spriteRenderer.sprite = assetLibrary.cornLong;
+            spriteRenderer.color = new Color(spriteRenderer.color.r,spriteRenderer.color.g,spriteRenderer.color.b,1);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player" && cornLevel >= 2)
+        {
+            PlayerManager playerManager = col.gameObject.GetComponent<PlayerManager>();
+
+            // Stop any corn growth
+            StopAllCoroutines();
+
+            if(cornLevel == 2 && playerManager.cornValue < 5f)
+            {
+                playerManager.cornValue += 0.01f;
+            }
+
+            if(cornLevel == 3 && playerManager.cornValue < 5f)
+            {
+                playerManager.cornValue += 0.03f;
+            }
+
+            // Trigger particle
+            cornParticle.Play();
+            
+            // Start growing from scratch again
+            SetCornlevel(0);
+        }
+    }
+
+    IEnumerator GrowCorn()
+    {
+        float growthCycleDuration = 10f;
+
+        while(cornLevel < 3)
+        {
+            yield return new WaitForSeconds(growthCycleDuration);
+            Debug.Log("Timer complete!");
+
+            SetCornlevel(cornLevel + 1);
+        }
+
+        Debug.Log("Corn has grown");
+    }
+}
