631f380c85c12d7e349a3b12a3ded698c788cb4a
harryelwell <107930302+harryelwell@users.noreply.github.com>
2023-01-07 11:23:43 +0000

    Improved Player Movement
    Swapped from rb.AddForce to rb.velocity, much better!

--- Ludum Dare 52 - Harvest/Assets/Code/Managers/PlayerManager.cs
+++ Ludum Dare 52 - Harvest/Assets/Code/Managers/PlayerManager.cs
@@ -11,12 +11,7 @@ public class PlayerManager : MonoBehaviour
     [Header("Player Settings")]
     public float playerSpeed;
     public float turnSpeed;
-    public float normalDrag;
-    public float turnDrag;
     public float cornValue; // min 0, max 3 (+0.3 each time you hit corn)
-    [Header("Gameplay Checks")]
-    float previousTurnValue;
-    IEnumerator activeDragCoroutine;
     
     void Awake()
     {
@@ -53,11 +48,11 @@ public class PlayerManager : MonoBehaviour
 
     void FixedUpdate()
     {
-        PlayerAcceleration();
-        PlayerRotation();
+        PlayerVelocityAcceleration();
+        PlayerVelocityRotation();
     }
 
-    void PlayerAcceleration()
+    void PlayerVelocityAcceleration()
     {
         float calculatedPlayerSpeed = playerSpeed * (1 + cornValue);
         //Debug.Log($"cornValue is {cornValue}, playerSpeed is {playerSpeed}, calculatedPlayerSpeed {calculatedPlayerSpeed}");
@@ -66,11 +61,11 @@ public class PlayerManager : MonoBehaviour
 
         if(accelerationValue > 0f)
         {
-            playerRigidbody.AddForce(transform.up * calculatedPlayerSpeed,ForceMode2D.Force);
+            playerRigidbody.velocity = transform.up * calculatedPlayerSpeed * Time.fixedDeltaTime;
         }
         else if(accelerationValue < 0f)
         {
-            playerRigidbody.AddForce(-transform.up * (calculatedPlayerSpeed * 0.3f),ForceMode2D.Force);
+            playerRigidbody.velocity = -transform.up * (calculatedPlayerSpeed * 0.3f) * Time.fixedDeltaTime;
         }
         else
         {
@@ -78,77 +73,18 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
-    void PlayerRotation()
+    void PlayerVelocityRotation()
     {
         float turnValue = playerActions.PlayerMap.Turn.ReadValue<float>();
-        //Debug.Log($"Turn value is: {turnValue}, previous turn value is {previousTurnValue}");
-        
-        if(previousTurnValue != turnValue)
-        {
-            Debug.Log($"Turn value is: {turnValue}, previous turn value is {previousTurnValue}");
-            
-            if(turnValue != 0f)
-            {
-                //playerRigidbody.drag = turnDrag;
-                Debug.Log("Apply additional drag!");
-                if(activeDragCoroutine != null)
-                {
-                    StopCoroutine(activeDragCoroutine);
-                }
-                activeDragCoroutine = ApplyTurnDrag(playerRigidbody.drag,turnDrag,CalculatedTurnDragDuration(normalDrag,turnDrag));
-                StartCoroutine(activeDragCoroutine);
-            }
 
-            if(turnValue == 0f)
-            {
-                //playerRigidbody.drag = normalDrag;
-                Debug.Log("Stop applying drag and return to normal.");
-                if(activeDragCoroutine != null)
-                {
-                    StopCoroutine(activeDragCoroutine);
-                }
-                activeDragCoroutine = ApplyTurnDrag(playerRigidbody.drag,normalDrag,CalculatedTurnDragDuration(turnDrag,normalDrag));
-                StartCoroutine(activeDragCoroutine);
-            }
-        }
-
-        // reverse the rotation if player is holding reverse
         float calculatedTurnValue = turnValue;
         if(playerActions.PlayerMap.Accelerate.ReadValue<float>() == -1f)
         {
             calculatedTurnValue = -turnValue;
         }
 
-        transform.Rotate(0,0,calculatedTurnValue * turnSpeed * Time.deltaTime);
+        calculatedTurnValue *= (1 + (cornValue * 0.5f));
 
-        previousTurnValue = turnValue;
-    }
-
-    float CalculatedTurnDragDuration(float intendedStartingDrag, float targetDrag)
-    {
-        float calculatedDuration = 1f;
-
-        float expectedDifference = targetDrag - intendedStartingDrag;
-        float actualDifference = playerRigidbody.drag - intendedStartingDrag;
-        float progressComplete = actualDifference / expectedDifference;
-        float amountToSubtract = calculatedDuration * progressComplete;
-
-        calculatedDuration -= amountToSubtract; 
-
-        return calculatedDuration;
-    }
-
-    IEnumerator ApplyTurnDrag(float startingDrag, float endingDrag, float lerpDuration)
-    {
-        Debug.Log($"Starting ApplyTurnDrag coroutine. Values are startingDrag: {startingDrag}, endingDrag: {endingDrag}, lerpDuration: {lerpDuration}.");
-        float timeElapsed = 0f;
-        while(timeElapsed < lerpDuration)
-        {
-            playerRigidbody.drag = Mathf.Lerp(startingDrag,endingDrag,timeElapsed);
-            timeElapsed = timeElapsed + Time.deltaTime / lerpDuration;
-            yield return new WaitForEndOfFrame();
-        }
-        
-        playerRigidbody.drag = endingDrag;
+        transform.Rotate(0,0,calculatedTurnValue * turnSpeed * Time.fixedDeltaTime);
     }
 }
